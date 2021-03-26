de82dd890f2d499c7b5104dc1881500a74c3c613
zhangquankang <spiritzhang@webank.com>
2021-03-27 00:40:26 +0800

    Initial commit

---
+++ src/main/java/cn/spirit/transpay/TransPayApplication.java
@@ -0,0 +1,13 @@
+package cn.spirit.transpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/spirit/transpay/TransPayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.spirit.transpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
